6b7471325a101adb59a2952f9fae35652245e677
DimitrijW <80290778+DimitrijW@users.noreply.github.com>
2021-03-11 16:23:34 +0100

    Add files via upload
    comment

--- PID-Control-Test.ino
+++ PID-Control-Test.ino
@@ -1,5 +1,8 @@
 /*
- *    1
+        Programm: PID
+        val_poti - value potentiometer
+        acc_rawX, acc_rawY, acc_rawZ - accelerometer raw
+        gyr_rawX, gyr_rawY, gyr_rawZ - gyroskop raw
 */
 #include <Wire.h>
 #include <Servo.h>
@@ -24,21 +27,20 @@ double Ki = 0;
 double Kd = 6;
 int val_poti;
 
-double throttle = val_poti; //initial value of throttle to the motors
-float desired_angle = 0; //This is the angle in which we whant the
-                         //balance to stay steady
+double throttle = val_poti;
+float desired_angle = 0; 
 
 
 void setup() {
-  Wire.begin(); //begin the wire comunication
-  Wire.beginTransmission(0x68);
-  Wire.write(0x6B);
-  Wire.write(0);
+  Wire.begin(); 
+  Wire.beginTransmission(0x68); // I2C address of the MPU-6050
+  Wire.write(0x6B); // 0x6B | PWR_MGMT_1 --> MPU-6050 Register Map
+  Wire.write(0);  // set to zero(wakes up the MPU 6050)
   Wire.endTransmission(true);
   
   Serial.begin(250000);
-  motor_right.attach(3); //attatch the right motor to pin 3
-  motor_left.attach(5);  //attatch the left motor to pin 5
+  motor_right.attach(3); // attatch the right motor to pin 3
+  motor_left.attach(5);  // attatch the left motor to pin 5
 
   time = millis();
   
@@ -62,25 +64,26 @@ void loop() {
   time_elapsed = (time - time_previous) / 1000; 
 
   Wire.beginTransmission(0x68);
-  Wire.write(0x3B);
+  Wire.write(0x3B); // starting with register 0x3B | ACCEL_XOUT_H --> MPU-6050 Register Map
   Wire.endTransmission(false);
-  Wire.requestFrom(0x68,6,true); 
+  Wire.requestFrom(0x68,6,true);  // request a total of 6 registers
     
-  acc_rawX = Wire.read()<<8|Wire.read();
-  acc_rawY = Wire.read()<<8|Wire.read();
-  acc_rawZ = Wire.read()<<8|Wire.read();
+  acc_rawX = Wire.read()<<8|Wire.read();  // 0x3B | 59 | ACCEL_XOUT_H and 0x3C | 60 | ACCEL_XOUT_L
+  acc_rawY = Wire.read()<<8|Wire.read();  // 0x3D | 61 | ACCEL_YOUT_H and 0x3E | 62 | ACCEL_YOUT_L
+  acc_rawZ = Wire.read()<<8|Wire.read();  // 0x3F | 63 | ACCEL_ZOUT_H and 0x40 | 64 | ACCEL_ZOUT_L
 
   acceleration_angle[0] = atan((acc_rawY / 16384.0) / sqrt(pow((acc_rawX / 16384.0),2) + pow((acc_rawZ / 16384.0),2))) * rad_to_deg;
   acceleration_angle[1] = atan(-1 * (acc_rawX / 16384.0) / sqrt(pow((acc_rawY / 16384.0),2) + pow((acc_rawZ / 16384.0),2))) * rad_to_deg;
  
   Wire.beginTransmission(0x68);
-  Wire.write(0x43);
+  Wire.write(0x43); // starting with register 0x43 | GYRO_XOUT_H --> MPU-6050 Register Map
   Wire.endTransmission(false);
-  Wire.requestFrom(0x68,4,true);
+  Wire.requestFrom(0x68,6,true);
    
-  gyr_rawX = Wire.read()<<8|Wire.read();
-  gyr_rawY = Wire.read()<<8|Wire.read();
-
+  gyr_rawX = Wire.read()<<8|Wire.read();  // 0x43 | 67 | GYRO_XOUT_H and 0x44 | 68 | GYRO_XOUT_L
+  gyr_rawY = Wire.read()<<8|Wire.read();  // 0x45 | 69 | GYRO_YOUT_H and 0x46 | 70 | GYRO_YOUT_L
+  //gyr_rawZ = Wire.read()<<8|Wire.read();  // 0x47 | 71 | GYRO_ZOUT_H and 0x48 | 72 | GYRO_ZOUT_L
+  
   gyro_angle[0] = gyr_rawX / 131.0; 
   gyro_angle[1] = gyr_rawY / 131.0;
 
